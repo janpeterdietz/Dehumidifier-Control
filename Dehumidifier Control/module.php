63aa1a34894317b25d33f5f3783fcb5e06358558
janpeterdietz <jan-peter.dietz@web.de>
2024-10-04 20:35:01 +0200

    Update module.php

--- Dehumidifier Control/module.php
+++ Dehumidifier Control/module.php
@@ -236,7 +236,7 @@ declare(strict_types=1);
 			} // end Switch
 			
 			$this->WriteAttributeString('Controlstate', $trockner_control_state );
-			$this->LogMessage(" Controlstate ". $ID_Switch . " ".  IPS_GetName( $ID_Switch) . " ".  $trockner_control_state, KL_NOTIFY);
+			//$this->LogMessage(" Controlstate ". $ID_Switch . " ".  IPS_GetName( $ID_Switch) . " ".  $trockner_control_state, KL_NOTIFY);
 
 			if ( $WindowState != "geschloßen")
 			{ 
